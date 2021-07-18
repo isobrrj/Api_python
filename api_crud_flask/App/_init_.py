9db62e1752c1e45bb561a58f4a3c281515966bb5
igor <estudostec@hotmail.com>
2021-07-18 14:43:45 -0300

    python

---
+++ api_crud_flask/App/_init_.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_migrate import Migrate
+from .model import configure as config_db
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/crudizin.db'
+
+    config_db(app)
+
+    Migrate(app, app.db)
+
+    return app
+
